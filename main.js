489492dab583d016397e4e5a59b8e76867ea25f9
Berke <69055194+fsb3rke@users.noreply.github.com>
2022-07-24 23:00:22 +0300

    Update main.js

--- main.js
+++ main.js
@@ -7,18 +7,19 @@ function fetchUser(id, res) {
     m_user = {
         userId: "",
         username: "",
-        created_at: "",
+        createdAt: "",
         avatarUrl: "",
     }
     client.users.fetch(id).then(user => {
         m_user.username = user.username
         m_user.userId = user.id
-        m_user.created_at = String(user.createdAt)
+        m_user.createdAt = String(user.createdAt)
         m_user.avatarUrl = user.avatarURL().replace(".webp", ".png?size=1024")
         //console.log(`\tID: ${m_user.userId}\n\tUSERNAME: ${m_user.username}\n\tCREATED: ${m_user.created_at}\n\tAVATAR: ${m_user.avatarUrl}`, m_user)
         
-        let page_source = `<center><div><img style="width: 8rem; height: 8rem;" src="${m_user.avatarUrl}"></div><br><br><br><br><div>ID: ${m_user.userId}</div><br><br><div>username: ${m_user.username}</div><br><br><div>created: ${m_user.created_at}</div><br><br>`
-        res.send(page_source)
+        //let html_page_source = `<center><div><img style="width: 8rem; height: 8rem;" src="${m_user.avatarUrl}"></div><br><br><br><br><div>ID: ${m_user.userId}</div><br><br><div>username: ${m_user.username}</div><br><br><div>created: ${m_user.created_at}</div><br><br>`
+        let json_page_source = JSON.stringify(m_user)
+        res.send(json_page_source)
     })
     
 }
@@ -34,26 +35,6 @@ client.on("ready", () => {
     app.get("/api/users/:id", (req, res) => {
         console.log(req.id);
         fetchUser(req.id, res)
-        // res.send(```
-        // <center>
-        // <div>
-        // <img src="${user.avatarUrl}">
-        // </div>
-        // <br><br><br><br>
-        // <div>
-        // ID: ${user.userId}
-        // </div>
-        // <br><br>
-        // <div>
-        // username: ${user.username}
-        // </div>
-        // <br><br>
-        // <div>
-        // created: ${user.created_at}
-        // </div>
-        // <br><br>
-        
-        // ```)
     })
 
     const port = process.env.PORT || 8080;
@@ -65,6 +46,6 @@ client.on("ready", () => {
 
 
 
-client.login("")
+client.login("discord_token")
 
 module.exports.fetchUser = fetchUser
